3b72add8d809903f4006c6e3632321d21e401c63
Alaa Abu Mussa <166786217+alaaabumusa@users.noreply.github.com>
2024-11-01 01:59:51 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const getCategories = async () => {  // بجيب الداتا
 
     const { data } = await axios.get(`https://dummyjson.com/products/category-list`);
-  
+
     return data;
 }
 
@@ -34,7 +34,7 @@ const displayCategories = async () => {  // بعرض الداتا
 }
 
 const getProducts = async (page) => {  // بجيب الداتا
-    const skip = (page - 1) * 9 ;
+    const skip = (page - 1) * 9;
     const { data } = await axios.get(`https://dummyjson.com/products?limit=9&skip=${skip}`);
     return data;
 }
@@ -45,32 +45,32 @@ const displayProducts = async (page = 1) => {  // بعرض الداتا
 
     try {
         const data = await getProducts(page);
-        const numberOfPages= Math.ceil(data.total / 9) ;
+        const numberOfPages = Math.ceil(data.total / 9);
         console.log(page);
         const result = data.products.map((product) => {
-        return ` <div class="product">
+            return ` <div class="product">
         <img src =${product.thumbnail} alt =${product.title} />
         <h3>${product.title}</h3>
         
         <span>$${product.price}</span>
         </div>`
 
-    }).join(' ');
-    document.querySelector(".products .row").innerHTML = result;
+        }).join(' ');
+        document.querySelector(".products .row").innerHTML = result;
 
-    let paginationLink = `<li class="page-item"><button class="page-link" onclick="displayProducts(${page - 1})" ${page === 1 ? 'disabled' : ''}>&laquo;</button></li>`;
+        let paginationLink = `<li class="page-item"><button class="page-link" onclick="displayProducts(${page - 1})" ${page === 1 ? 'disabled' : ''}>&laquo;</button></li>`;
 
-for (let i = 1; i <= numberOfPages; i++) {
-    paginationLink += `<li class="page-item ${i === page ? 'active' : ''}"><button onclick="displayProducts(${i})" class="page-link">${i}</button></li>`;
-}
+        for (let i = 1; i <= numberOfPages; i++) {
+            paginationLink += `<li class="page-item ${i === page ? 'active' : ''}"><button onclick="displayProducts(${i})" class="page-link">${i}</button></li>`;
+        }
 
-paginationLink += `<li class="page-item"><button class="page-link" onclick="displayProducts(${page + 1})" ${page === numberOfPages ? 'disabled' : ''}>&raquo;</button></li>`;
+        paginationLink += `<li class="page-item"><button class="page-link" onclick="displayProducts(${page + 1})" ${page === numberOfPages ? 'disabled' : ''}>&raquo;</button></li>`;
 
-       
+
         console.log(paginationLink);
 
-        document.querySelector(".pagination").innerHTML =paginationLink;
-}
+        document.querySelector(".pagination").innerHTML = paginationLink;
+    }
 
     catch (error) {
         console.error(error);
@@ -80,41 +80,41 @@ paginationLink += `<li class="page-item"><button class="page-link" onclick="disp
     }
 }
 
-    displayCategories();
-    displayProducts();
+displayCategories();
+displayProducts();
 
 
-    window.onscroll = function(){
-        const nav = document.querySelector(".header");
-        const categories = document.querySelector(".categories");
-        
-        if (window.scrollY > categories.offsetTop) {
-          nav.classList.add("scrolNavbar");
-        }else{
-            nav.classList.remove("scrolNavbar");
-        }
+window.onscroll = function () {
+    const nav = document.querySelector(".header");
+    const categories = document.querySelector(".categories");
+
+    if (window.scrollY > categories.offsetTop) {
+        nav.classList.add("scrolNavbar");
+    } else {
+        nav.classList.remove("scrolNavbar");
     }
+}
 
-    const countDown =()=>{
-        const countDownDate = new Date("2025-2-5 23:59:59").getTime();
-        const now = new Date().getTime();
-        const distance = countDownDate - now;
+const countDown = () => {
+    const countDownDate = new Date("2025-2-5 23:59:59").getTime();
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
 
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-       
-
-        document.querySelector(".days").textContent = days;
-        document.querySelector(".hours").textContent = hours;
-        document.querySelector(".minutes").textContent = minutes;
-        document.querySelector(".seconds").textContent = seconds;
-    }
-        
-  
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 
-    setInterval(()=>{
-        countDown();
-    },1000)
+    document.querySelector(".days").textContent = days;
+    document.querySelector(".hours").textContent = hours;
+    document.querySelector(".minutes").textContent = minutes;
+    document.querySelector(".seconds").textContent = seconds;
+}
+
+setInterval(() => {
+    countDown();
+}, 1000)
+
+
+
